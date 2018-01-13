8ca574cc66e3a1f9288e8237150a728ce11495ff
rpetchen <rpetchen@gmu.edu>
2018-01-13 13:16:49 -0500

    feat: AsyncStorage refactoring

--- src/components/DeckListView.js
+++ src/components/DeckListView.js
@@ -1,41 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import {connect} from 'react-redux'
-import {getDecks} from '../actions/index'
+import { StyleSheet, Text, View, FlatList, AsyncStorage } from 'react-native';
 import Card from './common/Card'
-
+import {setDummyData, getDecks} from '../../utils/api'
 
 class DeckListView extends React.Component {
 
-
+state = {
+ decks: {}
+}
 
 componentDidMount(){
-this.props.getDecks()
+
+ setDummyData()
+ .then(getDecks((key, asyncData) => {
+   this.setState({...this.state, decks:{
+    ...this.state.decks,
+    [key]: asyncData
+ }})
+}))
+ AsyncStorage.getAllKeys().then((k)=>{
+  const length = k.length 
+  this.setState({length})
+})
 }
+
+
 
 onPress=(deck)=>{
   this.props.navigation.navigate('DeckView', {deck})
 }
 
-  render() {
-  	const {decks} = this.props
+objTitle = (item) =>{
+  let objItem = JSON.parse(item)
+  return objItem.title
+}
 
-  	if (!decks) {
+objQuestionCount = (item) => {
+  let objItem = JSON.parse(item)
+  return objItem.questions.length
+}
+
+  render() {
+  	const {decks, length} = this.state
+
+  	if ( Object.keys(decks).length !== length ) {
   		return <Text> Loading </Text>
   	}
 
   	const deckList = Object.keys(decks).map((d) => {return decks[d]})
-
+  
     return (
-
       <FlatList
                 data={deckList}
+                extraData={this.state} 
                 renderItem={({item}) =>(
-                    
-                <Card title = {item.title} 
-                length = {item.questions.length} 
+
+                <Card title = {this.objTitle(item)} 
+                length = {this.objQuestionCount(item)} 
                 onPress={this.onPress}
                 />
+               
                 )}
                 keyExtractor={(item, index) => index}
             />
@@ -44,8 +68,4 @@ onPress=(deck)=>{
 }
 
 
-const mapStateToProps = ({decks}) =>{
-	return {decks : decks}
-}
-
-export default connect(mapStateToProps, {getDecks})(DeckListView)
+export default DeckListView
